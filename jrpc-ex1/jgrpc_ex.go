e7c8404f23fc06f987e90fd57d5e0a4574f45f74
Laurent Demailly <ldemailly@gmail.com>
2022-09-06 17:51:45 -0700

    refine example a bit to bit moderately more useful (a bit curl like)

--- jrpc-ex1/jgrpc_ex.go
+++ jrpc-ex1/jgrpc_ex.go
@@ -2,17 +2,20 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"log"
+	"os"
 
 	"fortio.org/fortio/jrpc"
 	"fortio.org/fortio/version"
 )
 
 func main() {
+	// Assumes some fortio echo debug server is running. pass -url localhost:8080/?status=503 for error case for instance.
+	url := flag.String("url", "http://localhost:8080/debug", "url to fetch")
 	flag.Parse()
-	// Assume some fortio echo debug server is running.
-	code, bytes, err := jrpc.FetchURL("http://localhost:8080/debug")
+	code, bytes, err := jrpc.FetchURL(*url)
 	_, _, fullV := version.FromBuildInfo()
-	fmt.Print(fullV)
-	fmt.Printf("err %v, code %d, bytes:\n%s", err, code, string(bytes))
+	log.Printf("Running: %s", fullV)
+	log.Printf("err %v, code %d, bytes (len %d):", err, code, len(bytes))
+	os.Stdout.Write(bytes)
 }
